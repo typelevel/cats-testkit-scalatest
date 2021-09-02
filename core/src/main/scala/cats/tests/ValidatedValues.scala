f655fab8415d8fb805a9bfc5c4bc94ca7eacd333
Nathan Fischer <nathan@nrktkt.tk>
2021-09-01 18:24:55 -0700

    fix position

--- core/src/main/scala/cats/tests/ValidatedValues.scala
+++ core/src/main/scala/cats/tests/ValidatedValues.scala
@@ -2,17 +2,19 @@ package cats.tests
 
 import cats.data.Validated
 import cats.data.Validated.{Invalid, Valid}
-import org.scalatest.Assertions.fail
+import org.scalactic.source.Position
+import org.scalatest.exceptions.TestFailedException
 
 trait ValidatedValues {
-  implicit class ValueForValidated[E, A](result: Validated[E, A]) {
+  implicit class ValueForValidated[E, A](result: Validated[E, A])(implicit pos: Position) {
 
     /**
      * @return the value of this Validated if it is Valid, else fail the test
      */
     def value: A = result match {
-      case Invalid(e) => fail(s"Validation failed with $e")
-      case Valid(a)   => a
+      case Invalid(e: Throwable) => throw new TestFailedException(_ => Some(s"Validation failed"), Some(e), pos)
+      case Invalid(e)            => throw new TestFailedException(_ => Some(s"Validation failed with $e"), None, pos)
+      case Valid(a)              => a
     }
   }
 }
